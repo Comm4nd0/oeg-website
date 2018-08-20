cce1a59d0d024490ac2a0be29ad0ac8f0d8c5e2c
Marco Baldanaza <marcobaldanza@hotmail.co.uk>
2018-08-20 20:35:00 +0100

    i'm a hero

--- pedigree/urls.py
+++ pedigree/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.search, name='pedigree'),
     path('results/', views.search_res, name='results'),
-    path('preview/<str:reg_no>/',views.view_from_admin, name='preview'),
+    path('preview/<str:reg_no>/', views.view_from_admin, name='preview'),
     path('breeder/<str:breeder>/', views.breeder, name='breeder'),
-    path('pedigree_csv/', views.goat_csv,)
- ]
+    path('pedigree_csv/', views.goat_csv, ),
+    path('breeder_csv/', views.breeder_csv, )
+]
--- pedigree/views.py
+++ pedigree/views.py
@@ -145,3 +145,30 @@ def goat_csv(request):
                          row.third_prize])
 
     return response
+
+
+def breeder_csv(request):
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="breeder_db.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['prefix',
+                     'contact_name',
+                     'address',
+                     'phone_number1',
+                     'phone_number2',
+                     'email',
+                     'active'])
+    breeder = Breeder.objects
+    for row in breeder.all():
+        writer.writerow([row.prefix,
+                         row.contact_name,
+                         row.address,
+                         row.phone_number1,
+                         row.phone_number2,
+                         row.email,
+                         row.active
+                         ])
+
+    return response
